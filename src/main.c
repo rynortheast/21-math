2f306ed92fb7c2cfb2f2a45a24204e536b3c20b5
Ryo Vamirion <rynortheast@yandex.ru>
2022-07-31 03:21:22 +0300

    Добавлена начальная структура проекта

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include "s21_math.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    
+	printf("TEST_1 - %d", 304);
+	printf("TEST_2 - %d", 304);
+	
+    return 0;
+}
---
+++ src/s21_math.h
@@ -0,0 +1,8 @@
+#ifndef SRC_S21_MATH_H_
+#define SRC_S21_MATH_H_
+
+#include <stdio.h>
+
+
+
+#endif  // SRC_S21_MATH_H
