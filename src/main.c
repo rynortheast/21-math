a56cbe12a390f74c0e092027dc9e46f42f313163
Ryo Vamirion <rynortheast@yandex.ru>
2022-08-09 02:24:33 +0300

    Реализована функция s21_atan

--- src/main.c
+++ src/main.c
@@ -5,16 +5,33 @@
 
 int main(void) {
     
-	printf("TEST_1 - %Lf\n", tanl(0));
-	printf("TEST_2 - %Lf\n\n", s21_tan(0));
-	printf("TEST_3 - %Lf\n", tanl(1.999));
-	printf("TEST_4 - %Lf\n\n", s21_tan(1.999));
-	printf("TEST_5 - %Lf\n", tanl(-1.676));
-	printf("TEST_6 - %Lf\n\n", s21_tan(-1.676));
-	printf("TEST_7 - %Lf\n", tanl(5.123));
-	printf("TEST_8 - %Lf\n\n", s21_tan(5.123));
-	printf("TEST_9 - %Lf\n", tanl(-5.3123));
-	printf("TEST_0 - %Lf", s21_tan(-5.3123));
+	// printf("TEST_1 - %Lf\n", tanl(0));
+	// printf("TEST_1 - %Lf\n\n", s21_tan(0));
+	// printf("TEST_2 - %Lf\n", tanl(1.999));
+	// printf("TEST_2 - %Lf\n\n", s21_tan(1.999));
+	// printf("TEST_3 - %Lf\n", tanl(-1.676));
+	// printf("TEST_3 - %Lf\n\n", s21_tan(-1.676));
+	// printf("TEST_4 - %Lf\n", tanl(5.123));
+	// printf("TEST_4 - %Lf\n\n", s21_tan(5.123));
+	// printf("TEST_5 - %Lf\n", tanl(-5.3123));
+	// printf("TEST_5 - %Lf", s21_tan(-5.3123));
 	
+	printf("\n\nNEW_TEST - [%Lf] [%Lf]\n\n", asinl(-0.9), atanl((-0.9) / sqrt(1 - pow(-0.9, 2))));
+
+	// printf("TEST_6 - %Lf\n", atanl(-50));
+	// printf("TEST_6 - %Lf\n\n", s21_atan(-50));
+	// printf("TEST_7 - %Lf\n", atanl(50));
+	// printf("TEST_7 - %Lf\n\n", s21_atan(50));
+	// printf("TEST_8 - %Lf\n", atanl(-1));
+	// printf("TEST_8 - %Lf\n\n", s21_atan(-1));
+	// printf("TEST_9 - %Lf\n", atanl(1));
+	// printf("TEST_9 - %Lf\n\n", s21_atan(1));
+	// printf("TEST_10 - %Lf\n", atanl(-0.99));
+	// printf("TEST_10 - %Lf\n\n", s21_atan(-0.99));
+	// printf("TEST_11 - %Lf\n", atanl(0.99));
+	// printf("TEST_11 - %Lf\n\n", s21_atan(0.99));
+	// printf("TEST_12 - %Lf\n", atanl(0));
+	// printf("TEST_12 - %Lf\n\n", s21_atan(0));
+
     return 0;
 }
--- src/s21_atan.c
+++ src/s21_atan.c
@@ -1,5 +1,28 @@
 #include "s21_math.h"
+#include <math.h>
 
-long double atan(double x) {
+long double s21_atan(double num) {
+    long double result = 0;
+    int status = 1, sign = 1, aux = 0;
+    if (s21_isNAN(num) || s21_isINF(num)) {
+        status = 0;
+    } else {
+        long double valueTailor = (long double) num;
 
+        if (valueTailor < 0.0)
+            valueTailor *= (sign = (-1));
+
+        if (fabsl(valueTailor) > 1.0)
+            valueTailor = (aux = 1) / valueTailor;
+
+        if (valueTailor == 1.0) {
+            result = s21_PI / 4;
+        } else if (valueTailor == (-1.0)) {
+            result = -s21_PI / 4;
+        } else {
+            for (int x = 0; x < 1000; x += 1)
+                result += (pow(-1, x) * powl(valueTailor, (1 + (2 * x)))) / (1 + (2 * x));
+        }
+    }
+    return status ? (aux ? (sign * (s21_PI / 2 - result)) : (sign * result)) : s21_NAN;
 }
