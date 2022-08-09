c0f653ffd9211dcf9a629c114be3471c62a54e87
Ryo Vamirion <rynortheast@yandex.ru>
2022-08-09 03:00:15 +0300

    Реализована функция s21_acos

--- src/main.c
+++ src/main.c
@@ -16,22 +16,22 @@ int main(void) {
 	// printf("TEST_5 - %Lf\n", tanl(-5.3123));
 	// printf("TEST_5 - %Lf", s21_tan(-5.3123));
 	
-	printf("\n\nNEW_TEST - [%Lf] [%Lf]\n\n", asinl(-0.9), atanl((-0.9) / sqrt(1 - pow(-0.9, 2))));
+	printf("\n\nNEW_TEST - [%Lf] [%Lf]\n\n", acosl(0.9), atanl(sqrt(1 - pow(0.9, 2)) / (0.9)));
 
-	printf("TEST_6 - %Lf\n", asinl(-50));
-	printf("TEST_6 - %Lf\n\n", s21_asin(-50));
-	printf("TEST_7 - %Lf\n", asinl(50));
-	printf("TEST_7 - %Lf\n\n", s21_asin(50));
-	printf("TEST_8 - %Lf\n", asinl(-1));
-	printf("TEST_8 - %Lf\n\n", s21_asin(-1));
-	printf("TEST_9 - %Lf\n", asinl(1));
-	printf("TEST_9 - %Lf\n\n", s21_asin(1));
-	printf("TEST_10 - %Lf\n", asinl(-0.99));
-	printf("TEST_10 - %Lf\n\n", s21_asin(-0.99));
-	printf("TEST_11 - %Lf\n", asinl(0.99));
-	printf("TEST_11 - %Lf\n\n", s21_asin(0.99));
-	printf("TEST_12 - %Lf\n", asinl(0));
-	printf("TEST_12 - %Lf\n\n", s21_asin(0));
+	printf("TEST_6 - %Lf\n", acosl(-5));
+	printf("TEST_6 - %Lf\n\n", s21_acos(-5));
+	printf("TEST_7 - %Lf\n", acosl(5));
+	printf("TEST_7 - %Lf\n\n", s21_acos(5));
+	printf("TEST_8 - %Lf\n", acosl(-1));
+	printf("TEST_8 - %Lf\n\n", s21_acos(-1));
+	printf("TEST_9 - %Lf\n", acosl(1));
+	printf("TEST_9 - %Lf\n\n", s21_acos(1));
+	printf("TEST_10 - %Lf\n", acosl(-0.99));
+	printf("TEST_10 - %Lf\n\n", s21_acos(-0.99));
+	printf("TEST_11 - %Lf\n", acosl(0.99));
+	printf("TEST_11 - %Lf\n\n", s21_acos(0.99));
+	printf("TEST_12 - %Lf\n", acosl(0));
+	printf("TEST_12 - %Lf\n\n", s21_acos(0));
 
     return 0;
 }
