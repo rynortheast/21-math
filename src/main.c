4ba5df3dc062f66e72dbac7fa438a2aa4b0f3fda
Ryo Vamirion <rynortheast@yandex.ru>
2022-08-09 03:09:59 +0300

    Дополнен main файл тестами для всех реализованных функций

--- src/main.c
+++ src/main.c
@@ -5,18 +5,38 @@
 
 int main(void) {
     
-	// printf("TEST_1 - %Lf\n", tanl(0));
-	// printf("TEST_1 - %Lf\n\n", s21_tan(0));
-	// printf("TEST_2 - %Lf\n", tanl(1.999));
-	// printf("TEST_2 - %Lf\n\n", s21_tan(1.999));
-	// printf("TEST_3 - %Lf\n", tanl(-1.676));
-	// printf("TEST_3 - %Lf\n\n", s21_tan(-1.676));
-	// printf("TEST_4 - %Lf\n", tanl(5.123));
-	// printf("TEST_4 - %Lf\n\n", s21_tan(5.123));
-	// printf("TEST_5 - %Lf\n", tanl(-5.3123));
-	// printf("TEST_5 - %Lf", s21_tan(-5.3123));
-	
-	printf("\n\nNEW_TEST - [%Lf] [%Lf]\n\n", acosl(0.9), atanl(sqrt(1 - pow(0.9, 2)) / (0.9)));
+	printf("TEST_1 - %Lf\n", sinl(0));
+	printf("TEST_1 - %Lf\n\n", s21_sin(0));
+	printf("TEST_2 - %Lf\n", sinl(1.999));
+	printf("TEST_2 - %Lf\n\n", s21_sin(1.999));
+	printf("TEST_3 - %Lf\n", sinl(-1.676));
+	printf("TEST_3 - %Lf\n\n", s21_sin(-1.676));
+	printf("TEST_4 - %Lf\n", sinl(5.123));
+	printf("TEST_4 - %Lf\n\n", s21_sin(5.123));
+	printf("TEST_5 - %Lf\n", sinl(-5.3123));
+	printf("TEST_5 - %Lf\n\n", s21_sin(-5.3123));
+
+	printf("TEST_1 - %Lf\n", cosl(0));
+	printf("TEST_1 - %Lf\n\n", s21_cos(0));
+	printf("TEST_2 - %Lf\n", cosl(1.999));
+	printf("TEST_2 - %Lf\n\n", s21_cos(1.999));
+	printf("TEST_3 - %Lf\n", cosl(-1.676));
+	printf("TEST_3 - %Lf\n\n", s21_cos(-1.676));
+	printf("TEST_4 - %Lf\n", cosl(5.123));
+	printf("TEST_4 - %Lf\n\n", s21_cos(5.123));
+	printf("TEST_5 - %Lf\n", cosl(-5.3123));
+	printf("TEST_5 - %Lf\n\n", s21_cos(-5.3123));
+
+	printf("TEST_1 - %Lf\n", tanl(0));
+	printf("TEST_1 - %Lf\n\n", s21_tan(0));
+	printf("TEST_2 - %Lf\n", tanl(1.999));
+	printf("TEST_2 - %Lf\n\n", s21_tan(1.999));
+	printf("TEST_3 - %Lf\n", tanl(-1.676));
+	printf("TEST_3 - %Lf\n\n", s21_tan(-1.676));
+	printf("TEST_4 - %Lf\n", tanl(5.123));
+	printf("TEST_4 - %Lf\n\n", s21_tan(5.123));
+	printf("TEST_5 - %Lf\n", tanl(-5.3123));
+	printf("TEST_5 - %Lf\n\n", s21_tan(-5.3123));
 
 	printf("TEST_6 - %Lf\n", acosl(-5));
 	printf("TEST_6 - %Lf\n\n", s21_acos(-5));
@@ -32,6 +52,36 @@ int main(void) {
 	printf("TEST_11 - %Lf\n\n", s21_acos(0.99));
 	printf("TEST_12 - %Lf\n", acosl(0));
 	printf("TEST_12 - %Lf\n\n", s21_acos(0));
+
+	printf("TEST_6 - %Lf\n", asinl(-5));
+	printf("TEST_6 - %Lf\n\n", s21_asin(-5));
+	printf("TEST_7 - %Lf\n", asinl(5));
+	printf("TEST_7 - %Lf\n\n", s21_asin(5));
+	printf("TEST_8 - %Lf\n", asinl(-1));
+	printf("TEST_8 - %Lf\n\n", s21_asin(-1));
+	printf("TEST_9 - %Lf\n", asinl(1));
+	printf("TEST_9 - %Lf\n\n", s21_asin(1));
+	printf("TEST_10 - %Lf\n", asinl(-0.99));
+	printf("TEST_10 - %Lf\n\n", s21_asin(-0.99));
+	printf("TEST_11 - %Lf\n", asinl(0.99));
+	printf("TEST_11 - %Lf\n\n", s21_asin(0.99));
+	printf("TEST_12 - %Lf\n", asinl(0));
+	printf("TEST_12 - %Lf\n\n", s21_asin(0));
+
+	printf("TEST_6 - %Lf\n", atanl(-5));
+	printf("TEST_6 - %Lf\n\n", s21_atan(-5));
+	printf("TEST_7 - %Lf\n", atanl(5));
+	printf("TEST_7 - %Lf\n\n", s21_atan(5));
+	printf("TEST_8 - %Lf\n", atanl(-1));
+	printf("TEST_8 - %Lf\n\n", s21_atan(-1));
+	printf("TEST_9 - %Lf\n", atanl(1));
+	printf("TEST_9 - %Lf\n\n", s21_atan(1));
+	printf("TEST_10 - %Lf\n", atanl(-0.99));
+	printf("TEST_10 - %Lf\n\n", s21_atan(-0.99));
+	printf("TEST_11 - %Lf\n", atanl(0.99));
+	printf("TEST_11 - %Lf\n\n", s21_atan(0.99));
+	printf("TEST_12 - %Lf\n", atanl(0));
+	printf("TEST_12 - %Lf\n\n", s21_atan(0));
 
     return 0;
 }
