2bfc2f09b14bf8ffe6e28e3aa7fb682f7aa5c84f
Ryo Vamirion <rynortheast@yandex.ru>
2022-12-28 17:10:21 +0300

    Обновлен s21_math.h и Makefile

--- src/s21_math.h
+++ src/s21_math.h
@@ -1,15 +1,53 @@
 #ifndef SRC_S21_MATH_H_
 #define SRC_S21_MATH_H_
 
+#include <errno.h>
 #include <stdio.h>
-#include <math.h>
 
-#define s21_INF (__builtin_inff())
-#define s21_NAN (__builtin_nanf(""))
-#define s21_isNAN(x) __builtin_isnan(x)
-#define s21_isINF(x) __builtin_isinf(x)
+#define s21_PI 3.1415926535897932384626433832795
+#define S21_INF 1.0 / 0.0
+#define S21_NINF -S21_INF
+#define S21_NAN __builtin_nanf("0x7fc00000")
+#define S21_DBL_MAX 1.7976931348623158e+308
+#define s21_LOG_10 2.30258509299
+#define s21_LOG_2 0.69314718056
+#define is_fin(x) __builtin_isfinite(x)
+#define s21_isnan(x) __builtin_isnan(x)
+#define s21_isinf(x) __builtin_isinf(x)
 
-#define s21_PI 3.1415926535897932384
+int is_neg_inf(double x);
+
+int s21_abs(int x);
+long double s21_fabs(double x);
+
+long double s21_ceil(double x);
+long double process_ceil(double x, long double tmp);
+int check_ceil(long double x);
+
+long double s21_fmod(double x, double y);
+long double process_fmod(double x, double y);
+int check_fmod(double x, double y);
+
+long double s21_floor(double x);
+long double process_floor(double x);
+int check_floor(double x);
+
+long double s21_sqrt(double x);
+long double check_sqrt(double x);
+
+long double s21_exp(double x);
+long double process_exp(double x);
+int check_exp(double x, long double *res);
+
+long double s21_log(double x);
+long double process_log(double x);
+int check_log(double x, long double *res);
+
+long double s21_pow(double base, double exp);
+long double process_pow(double base, double exp);
+long double s21_powi(double base, double exp);
+long double s21_powf(double base, double exp);
+int pow_check(double base, double exp, long double *result);
 
 long double s21_acos(double x);
 long double s21_asin(double x);
@@ -18,4 +56,4 @@ long double s21_cos(double x);
 long double s21_sin(double x);
 long double s21_tan(double x);
 
-#endif  // SRC_S21_MATH_H
+#endif  // SRC_S21_MATH_H_
--- src/s21_tan.c
+++
@@ -1,8 +0,0 @@
-#include "s21_math.h"
-
-long double s21_tan(double num) {
-    int status = 1;
-    if (s21_isNAN(num) || s21_isINF(num))
-        status = 0;
-    return status ? (s21_sin(num) / s21_cos(num)) : s21_NAN;
-}
